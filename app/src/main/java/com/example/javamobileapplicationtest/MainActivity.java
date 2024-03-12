f1a7bc48aba25d6f42440661f990f16dcd430dbe
cheeseballDev <cheeseballbusinessemail@gmail.com>
2024-03-12 21:30:17 +0800

    MAIN UI UPDATE

--- app/src/main/java/com/example/javamobileapplicationtest/MainActivity.java
+++ app/src/main/java/com/example/javamobileapplicationtest/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.javamobileapplicationtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.AlertDialog;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
+
+import java.util.concurrent.CountedCompleter;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }
