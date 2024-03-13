89e9cd6afb5c3dbeb765000c76e58034389e43fd
cheeseballDev <cheeseballbusinessemail@gmail.com>
2024-03-14 02:40:37 +0800

    UPDATE V.5

---
+++ app/src/main/java/com/example/javamobileapplicationtest/ItemActivity.java
@@ -0,0 +1,24 @@
+package com.example.javamobileapplicationtest;
+
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.example.javamobileapplicationtest.databinding.ActivityItemBinding;
+
+public class ItemActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityItemBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
