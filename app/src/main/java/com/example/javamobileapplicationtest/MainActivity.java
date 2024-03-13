b5efb09c80e9b7f27031e2cdd6d2526c62fdd98d
cheeseballDev <cheeseballbusinessemail@gmail.com>
2024-03-14 01:14:16 +0800

    UPDATE V.3 im losing my fucking mind

--- app/src/main/java/com/example/javamobileapplicationtest/MainActivity.java
+++ app/src/main/java/com/example/javamobileapplicationtest/MainActivity.java
@@ -2,24 +2,49 @@ package com.example.javamobileapplicationtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.app.AlertDialog;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
 import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
 
+import com.example.javamobileapplicationtest.databinding.ActivityMainBinding;
+
 import java.util.concurrent.CountedCompleter;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
+        binding.bottomNav.setOnItemSelectedListener(item -> {
 
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.bookmark) {
+                replaceFragment(new BookmarkedFragment());
+            } else if (itemId == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.mainConstraintLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/javamobileapplicationtest/ProfileFragment.java
+++ app/src/main/java/com/example/javamobileapplicationtest/ProfileFragment.java
@@ -8,14 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class HomeFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
     View view;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
         return view;
     }
 }
