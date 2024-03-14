6f5f92c06112ff2b6602098a2dceb99d57f5b474
cheeseballDev <cheeseballbusinessemail@gmail.com>
2024-03-14 09:49:00 +0800

    UPDATE V.6

--- app/src/main/java/com/example/javamobileapplicationtest/HomeFragment.java
+++ app/src/main/java/com/example/javamobileapplicationtest/HomeFragment.java
@@ -2,9 +2,7 @@ package com.example.javamobileapplicationtest;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,11 +16,10 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        cartButton=findViewById(R.id.cartBtn);
         cartButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(HomeFragment.this, CartActivity.class);
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), CartActivity.class);
                 startActivity(intent);
 
             }
