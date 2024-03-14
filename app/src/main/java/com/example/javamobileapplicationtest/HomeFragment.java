dc0a07689e9cebe29dbe628eb2d99fed1e2a1a65
cheeseballDev <cheeseballbusinessemail@gmail.com>
2024-03-14 13:49:14 +0800

    UPDATE V.8

--- app/src/main/java/com/example/javamobileapplicationtest/HomeFragment.java
+++ app/src/main/java/com/example/javamobileapplicationtest/HomeFragment.java
@@ -1,36 +1,31 @@
 package com.example.javamobileapplicationtest;
 
-import android.content.Intent;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
     View view;
     Button cartButton;
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        cartButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), CartActivity.class);
-                startActivity(intent);
 
-            }
-        });
     }
 
 
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
         view = inflater.inflate(R.layout.fragment_home, container, false);
         return view;
     }
--- app/src/main/java/com/example/javamobileapplicationtest/MainActivity.java
+++ app/src/main/java/com/example/javamobileapplicationtest/MainActivity.java
@@ -1,20 +1,13 @@
 package com.example.javamobileapplicationtest;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.app.AlertDialog;
-import android.os.Bundle;
-import android.renderscript.ScriptGroup;
-import android.view.View;
-import android.content.Intent;
-import android.widget.Button;
-
 import com.example.javamobileapplicationtest.databinding.ActivityMainBinding;
-
-import java.util.concurrent.CountedCompleter;
 
 public class MainActivity extends AppCompatActivity {
 
